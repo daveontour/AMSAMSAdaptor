a56bf71b7787d9d46d19f4a2a60b2a0c7a19c2cc
Dave Burton <dave_on_tour@yahoo.com>
2021-11-26 22:34:08 +0400

    Start Slot to Table Mapping

---
+++ AMSAMSAdaptor/Transformers/TransformerFlightSlotToTableMapping.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AMSAMSAdaptor
+{
+    internal class TransformerStandSlotToTable : ITransformer
+    {
+        private string Table { get; set; }
+        public void SetConfig(XmlNode configNode)
+        {
+            Table = configNode.SelectSingleNode(".//Property")?.Attributes["table"]?.Value;
+        }
+
+        public object Transform(object input)
+        {
+            ModelFlight fl = (ModelFlight)input;
+            XmlDocument doc = fl.node.OwnerDocument;
+            XmlElement tv = doc.CreateElement("TableValue");
+            tv.SetAttribute("propertyName", "StandSlots");
+            foreach (XmlNode s in fl.node.SelectNodes(".//amsx-datatypes:StandSlot", fl.nsmgr))
+            {
+                XmlElement row = doc.CreateElement("Row");
+
+                tv.AppendChild(row);
+            }
+
+
+            return fl;
+        }
+    }
+}
